f8c22c1a4826a403f6bc898c437c932b4c632073
Luke Hudson <lukeletters@gmail.com>
2014-04-27 03:56:13 +0200

    initial

---
+++ README.md
@@ -0,0 +1,46 @@
+# Inkscape onionskin
+
+## Author
+Luke Hudson <github@speak.geek.nz>
+
+## Installation
+
+Place the following *.idx* and *.py* files from this package within your Inkscape extensions directory.
+Then reload Inkscape. You should see new options in the menus: `Extensions > Animation > Onionskin`
+### Files to copy to extensions directory (**Not** a subdirectory thereof)
+- `nz.geek.speak.onionskin.idx`
+- `nz.geek.speak.onionskin.py`
+
+See also [Keyboard shortcuts](#user-content-keyboard-shortcuts)
+
+This is probably one of the following:
+- *Windows*: `C:\Program Files\Inkscape\share\extensions`
+- *Linux*: `~/.config/inkscape/extensions` (or `/usr/share/inkscape/extensions`)
+- *OS X*: `/Applications/Inkscape.app/Contents/Resources/extensions`
+
+## Usage
+I wrote this to make creation of animations easier in Inkscape.
+
+This plugin provides a method of ['onion-skinning'](http://en.wikipedia.org/wiki/Onion_skinning) using Inkscape's layer opacity.
+
+Run the plugin and it will hide any layers above the current layer, make the current layer fully opaque and progressively fade out previous layers.
+
+## Keyboard shortcuts
+
+You can assign a keyboard shortcut to this plugin to make your life easier.
+To do this, you can copy the `default.xml` file from this package into the inkscape *keys* directory and change the key as desired.
+
+The keys directory should be:
+
+- *Windows*: `C:\Program Files\Inkscape\share\keys`
+- *Linux*: `~/.config/inkscape/keys` (or `/usr/share/inkscape/keys`)
+- *OS X*: `/Applications/Inkscape.app/Contents/Resources/keys`
+
+If you already have a custom keys file, simply add two lines at the end (as shown below) before the `</keys>` tag.
+
+
+~~~xml
+   <bind key="l" modifiers="Alt,Shift" action="nz.geek.speak.animation" display="true"/>
+   <bind key="L" modifiers="Alt,Shift" action="nz.geek.speak.animation" />
+~~~
+
---
+++ nzspeakgeek_onionskin.py
@@ -0,0 +1,105 @@
+import sys, inkex, simplestyle
+import gettext
+_ = gettext.gettext
+
+
+class Onionskin(inkex.Effect):
+    def __init__(self):
+        inkex.Effect.__init__(self)
+        self.OptionParser.add_option("--onion-layers",
+                        type="int", dest="onion_layers", default=2, 
+                        help="# of visible onion skin layers")
+        self.OptionParser.add_option("--onion-base",
+                        type="int", dest="onion_base", default=70, 
+                        help="Opacity(%) of first onionskin layer")
+        self.OptionParser.add_option("--onion-clear",
+                        type="inkbool", dest="onion_clear", default=False, 
+                        help="Overrides other options and removes all onionskins")
+
+    def debug(self, arg):
+        # print arg
+        # return inkex.debug(arg)
+        pass
+
+    def effect(self):
+        layers = self.document.xpath('/svg:svg/svg:g[@inkscape:groupmode="layer"]', namespaces=inkex.NSS)
+        current = self.current_layer
+        if current is None:
+            self.debug('No current_layer found')
+            return
+        if current.tag != inkex.addNS('g', 'svg'):
+            self.debug("current_layer wasn't a svg:g tag (Found %s)" % current.tag)
+            return
+
+        if self.options.onion_clear:
+            [self.show_layer(l, full_opaque=True) for l in layers]
+            return
+
+        # Determine the index of the current layer
+        current_index = -1
+        for i in range(0, len(layers)):
+            layer = layers[i]
+            if layer == current:
+                current_index = i
+
+        self.debug("Luke: %d layers, current is %d : %s" % (len(layers), current_index, [self.get_layer_name(x) for x in layers]))
+
+        if current_index < 0:
+            return
+
+        # Hide any layers above the current one
+        for i in range(len(layers)-1, current_index, -1):
+            self.hide_layer(layers[i])
+
+        self.show_layer(current, full_opaque=True)
+
+        # Fade out layers below current
+        opacity = self.options.onion_base / 100.0
+        fade_factor = 1.0  / (self.options.onion_layers+1)
+
+        self.debug("start at index %d, %.2f, fading by %.2f" % (current_index-1,opacity, fade_factor))
+
+        for i in range(current_index-1, -1, -1):
+            if opacity <= 0:
+                self.set_layer_opacity(layers[i], 1)
+                self.hide_layer(layers[i])
+            else:
+                self.set_layer_opacity(layers[i], opacity)
+                self.show_layer(layers[i])
+            opacity -= fade_factor
+
+    def get_layer_name(self, layer):
+        return layer.attrib[inkex.addNS('label', 'inkscape')]
+
+    def modify_layer_style(self, layer, style):
+        if (layer.attrib.has_key('style')):
+            currentStyle = simplestyle.parseStyle(layer.attrib['style'])
+            for k,v in style.items():
+                if v == None and currentStyle.has_key(k):
+                    del currentStyle[k]
+                else:
+                    currentStyle[k] = style[k]
+            style = currentStyle
+        layer.attrib['style'] = simplestyle.formatStyle(style)
+
+    def set_layer_opacity(self, layer, opacity):
+        self.debug('set_layer_opacity "%s" %.2f' % (self.get_layer_name(layer), opacity))
+        self.modify_layer_style(layer, {"opacity": opacity})
+
+    def hide_layer(self, layer):
+        self.debug('hide_layer "%s"' % self.get_layer_name(layer))
+        self.modify_layer_style(layer, {"display": 'none'})
+
+    def show_layer(self, layer, full_opaque=False):
+        self.debug('show_layer "%s"' % self.get_layer_name(layer))
+        style = {"display": None}
+        if full_opaque:
+            style['opacity'] = 1
+        self.modify_layer_style(layer, style)
+
+if __name__ == '__main__':
+    try:
+        e = Onionskin()
+        e.affect()
+    except:
+        inkex.errormsg("Exception: %s" % sys.exc_info()[0])
