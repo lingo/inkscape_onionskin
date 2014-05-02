2e3115c8a2ebc70455b351c49283a2fb20747ace
Luke Hudson <lukeletters@gmail.com>
2014-05-02 16:48:55 +0200

    UPDATE Layers Actions plugin
    Added opacity control
    Allow for 'no change' option in visibility and lock

--- nzgslayer.py
+++ nzgslayer.py
@@ -14,11 +14,20 @@ class NZGSLayer(nzgs.NZGSEffect):
     def __init__(self):
         nzgs.NZGSEffect.__init__(self)
         self.OptionParser.add_option("--layers-show",
-                        type="string", dest="show_all", default="show",
+                        type="string", dest="show_all", default="ignore",
                         help="Show all layers")
         self.OptionParser.add_option("--layers-lock",
-                        type="string", dest="lock_all", default="unlock",
+                        type="string", dest="lock_all", default="ignore",
                         help="Lock all layers")
+        self.OptionParser.add_option("--layers-change-opacity",
+                        type="inkbool", dest="change_opacity", default=False,
+                        help="Affect opacity")
+        self.OptionParser.add_option("--opacity-tab",
+                        action="store", type="string", dest="tab",
+                        help="This is ignored")
+        self.OptionParser.add_option("--layers-opacity",
+                        type="int", dest="opacity", default=100,
+                        help="If opacity is affected, change it to this value")
         self.OptionParser.add_option("--layers-recursive",
                         type="inkbool", dest="recursive", default=False,
                         help="Act on sublayers too")
@@ -28,23 +37,30 @@ class NZGSLayer(nzgs.NZGSEffect):
 
     def effect(self):
         self.debug(self.options)
-        show_all = True
-        if self.options.show_all == "hide":
-            show_all = False
-        lock_all = False
-        if self.options.lock_all == "lock":
-            lock_all = True
-        layers = self.get_layers(recursive = self.options.recursive)
-        frameLabel = -1
+        change_vis     = self.options.show_all != 'ignore'
+        change_lock    = self.options.lock_all != 'ignore'
+        change_opacity = self.options.change_opacity
+
+        show_all       = self.options.show_all == "show"
+        lock_all       = self.options.lock_all == "lock"
+
+        layers         = self.get_layers(recursive = self.options.recursive)
+        frameLabel     = -1
         for layer in layers:
-            if show_all:
-                self.show_layer(layer)
-            else:
-                self.hide_layer(layer)
-            if lock_all:
-                self.set_layer_lock(layer)
-            else:
-                self.set_layer_lock(layer, unlock=True)
+            if change_vis:
+                if show_all:
+                    self.show_layer(layer)
+                else:
+                    self.hide_layer(layer)
+            if change_lock:
+                if lock_all:
+                    self.set_layer_lock(layer)
+                else:
+                    self.set_layer_lock(layer, unlock=True)
+
+            if change_opacity:
+                self.modify_layer_style(layer, {"opacity": self.options.opacity / 100.0})
+
             if self.options.rename:
                 if self.is_toplevel_layer(layer):
                     frameLabel += 1
